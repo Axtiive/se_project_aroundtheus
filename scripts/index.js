34507d792f6abde293ac9722a2e61158c929e1fa
Rogelio <marquez.rogelio1995@gmail.com>
2026-07-14 17:50:07 -0700

    Final review Fixes

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,7 @@ const cardTemplate =
 /*Functions*/
 
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -78,7 +78,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 const profileCloseButton = document.querySelector(".modal__close");
